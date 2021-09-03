6d7f18f023936a3d82113077384fcd969e51c056
Ramsey Mahgoub <Ramsey.Mahgoub@cinch.co.uk>
2021-09-03 20:14:08 +0100

    UPDATE: Change linting and pretter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "jest"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
-    "plugin:react/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "no-debugger": "warn",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-debugger': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-console': 1,
+    'prefer-template': 1,
   },
   overrides: [
     {
-      files: ["*.test.ts"],
+      files: ['*.test.ts'],
       rules: {
-        "@typescript-eslint/no-non-null-assertion": 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
       },
     },
     {
-      files: ["*.js"],
+      files: ['*.js'],
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
+        '@typescript-eslint/no-var-requires': 'off',
       },
       env: {
         node: true,
@@ -38,7 +40,7 @@ module.exports = {
   ],
   settings: {
     react: {
-      version: "latest",
+      version: 'latest',
     },
   },
-};
+}
